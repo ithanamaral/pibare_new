79c77926be2a3fb667720d43d08905c1608d4d76
Íthan P. Amaral <ithanamaral@gmail.com>
2025-12-29 22:15:07 -0300

    add fonte ao devocional

--- src/components/devotionals.jsx
+++ src/components/devotionals.jsx
@@ -14,18 +14,18 @@ function Devotionals() {
         <section className="text">
           <p className="texto">
             Quando as pessoas participam de uma orquestra, elas perdem algo de
-            sua própria individualidade. Uma sinfonia não é uma performance solo.
-            Embora os músicos não percam suas identidades, eles são, no entanto,
-            integrados na orquestra. O grupo é mais significativo como um todo do
-            que um indivíduo por conta própria, e o coletivo produz algo que
-            nenhum músico individual poderia criar.
-            </p>
-            
+            sua própria individualidade. Uma sinfonia não é uma performance
+            solo. Embora os músicos não percam suas identidades, eles são, no
+            entanto, integrados na orquestra. O grupo é mais significativo como
+            um todo do que um indivíduo por conta própria, e o coletivo produz
+            algo que nenhum músico individual poderia criar.
+          </p>
+
           <p className="texto">
-            Paulo expressa uma ideia
-            semelhante quando escreve sobre “[sujeitar-vos] uns aos outros” —
-            embora aqui, é claro, o grupo não seja uma orquestra, mas a igreja.
-            </p>
+            Paulo expressa uma ideia semelhante quando escreve sobre
+            “[sujeitar-vos] uns aos outros” — embora aqui, é claro, o grupo não
+            seja uma orquestra, mas a igreja.
+          </p>
 
           <p className="texto">
             Embora possamos ter uma variedade de respostas ao conceito de
@@ -33,37 +33,47 @@ function Devotionals() {
             direta e frequente. Para Paulo, a unidade e a saúde da igreja
             dependiam de os cristãos entenderem a submissão corretamente e
             colocarem isso em prática entre si.
-            </p>
+          </p>
 
-            <p className="texto">Como é levar a sério a questão da
-            submissão mútua dos crentes? Em parte, significa que cada um de nós
-            percebe não ter o menor motivo para se sentir excessivamente
-            satisfeito consigo mesmo ou superior a outra pessoa. Em outras
-            palavras, demonstramos submissão mútua revestindo-nos de humildade.
-            Isso é dificultado, é claro, pelo nosso orgulho — um grande desafio
-            que todos enfrentamos e é intensificado por vivermos em uma cultura
-            que está constantemente nos pressionando para a dianteira.
-            </p>
+          <p className="texto">
+            Como é levar a sério a questão da submissão mútua dos crentes? Em
+            parte, significa que cada um de nós percebe não ter o menor motivo
+            para se sentir excessivamente satisfeito consigo mesmo ou superior a
+            outra pessoa. Em outras palavras, demonstramos submissão mútua
+            revestindo-nos de humildade. Isso é dificultado, é claro, pelo nosso
+            orgulho — um grande desafio que todos enfrentamos e é intensificado
+            por vivermos em uma cultura que está constantemente nos pressionando
+            para a dianteira.
+          </p>
 
-            <p className="texto">No entanto, a igreja deve se destacar nesse tipo de ambiente. Como povo de Deus,
-            entendemos que não podemos nem sequer acordar de manhã sem a
-            capacitação dele. O fato é que somos totalmente dependentes dele (At
-            17.24-25). O Evangelho é a chave para a verdadeira humildade, pois nos
-            lembra que Deus fez por nós em Jesus aquilo de que mais precisamos e
-            que somos totalmente incapazes de fazer por nós mesmos.
-            </p>
+          <p className="texto">
+            No entanto, a igreja deve se destacar nesse tipo de ambiente. Como
+            povo de Deus, entendemos que não podemos nem sequer acordar de manhã
+            sem a capacitação dele. O fato é que somos totalmente dependentes
+            dele (At 17.24-25). O Evangelho é a chave para a verdadeira
+            humildade, pois nos lembra que Deus fez por nós em Jesus aquilo de
+            que mais precisamos e que somos totalmente incapazes de fazer por
+            nós mesmos.
+          </p>
 
-            <p className="texto">A verdadeira humildade não é autodepreciação; é liberdade de nós mesmos. É a
-            liberdade de sermos nós mesmos e nos esquecermos de nós mesmos. É a
-            liberdade que vem de saber que não somos o centro do universo. Quando
-            você mantém tal humildade em vista, está preparado para se submeter
-            aos outros — para trazer tudo o que você é e usá-lo para servir ao bem
-            maior, sob a direção de outros, com os interesses dos outros como sua
-            prioridade. Então sua igreja pode produzir algo bonito — uma
-            comunidade que exibe o Evangelho. Portanto, não espere que os outros
-            em sua igreja sejam esse tipo de cristão. Hoje, humildemente, decida
-            que você será esse cristão.
-            </p>
+          <p className="texto">
+            A verdadeira humildade não é autodepreciação; é liberdade de nós
+            mesmos. É a liberdade de sermos nós mesmos e nos esquecermos de nós
+            mesmos. É a liberdade que vem de saber que não somos o centro do
+            universo. Quando você mantém tal humildade em vista, está preparado
+            para se submeter aos outros — para trazer tudo o que você é e usá-lo
+            para servir ao bem maior, sob a direção de outros, com os interesses
+            dos outros como sua prioridade. Então sua igreja pode produzir algo
+            bonito — uma comunidade que exibe o Evangelho. Portanto, não espere
+            que os outros em sua igreja sejam esse tipo de cristão. Hoje,
+            humildemente, decida que você será esse cristão.
+          </p>
+          <p>
+            Fonte:{" "}
+            <a href="https://voltemosaoevangelho.com/blog/devocionais/submissao-e-humildade-efesios-5-20-21/">
+              https://voltemosaoevangelho.com/blog/devocionais/submissao-e-humildade-efesios-5-20-21/
+            </a>
+          </p>
         </section>
       </main>
     </>
