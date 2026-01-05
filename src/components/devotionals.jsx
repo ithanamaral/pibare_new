51d2d5ef77fee8eda8839c08d430ed688513bba2
Íthan P. Amaral <ithanamaral@gmail.com>
2026-01-05 16:57:47 -0300

    atualiza devocional

--- src/components/devotionals.jsx
+++ src/components/devotionals.jsx
@@ -5,73 +5,51 @@ function Devotionals() {
   return (
     <>
       <main className="devotionals">
-        <h1>Submissão e humildade (Efésios 5.20-21)</h1>
+        <h1>Calmo Repouso (Oséias 2.18)</h1>
         <p className="reflexao">
-          Dando sempre graças por tudo a nosso Deus e Pai, em nome de nosso
-          Senhor Jesus Cristo, sujeitando-vos uns aos outros no temor de Cristo.
-          (Ef 5.20-21)
+          "E naquele dia farei por eles aliança com as feras do campo, e com as
+          aves do céu, e com os répteis da terra; e da terra quebrarei o arco, e
+          a espada, e a guerra, e os farei deitar em segurança." (Oséias 2:18)
         </p>
         <section className="text">
           <p className="texto">
-            Quando as pessoas participam de uma orquestra, elas perdem algo de
-            sua própria individualidade. Uma sinfonia não é uma performance
-            solo. Embora os músicos não percam suas identidades, eles são, no
-            entanto, integrados na orquestra. O grupo é mais significativo como
-            um todo do que um indivíduo por conta própria, e o coletivo produz
-            algo que nenhum músico individual poderia criar.
+            Sim, os crentes precisam desfrutar de paz. A passagem de onde
+            retiramos essa graciosa promessa fala sobre paz ” com as
+            bestas-feras do campo, e com as aves do céu, e com os répteis da
+            terra”. Isto significa paz com os inimigos terrenos, com males
+            misteriosos e livre de alguns aborrecimentos! Qualquer dessas coisas
+            pode nos impedir de descansar, mas nenhuma delas conseguirá fazê-lo.
+            O Senhor destruirá as coisas que ameaçam seu povo – “Tirarei … o
+            arco, e a espada, e a guerra”. A paz será realmente profunda, quando
+            todos os instrumentos de inquietação forem despedaçados.
           </p>
 
           <p className="texto">
-            Paulo expressa uma ideia semelhante quando escreve sobre
-            “[sujeitar-vos] uns aos outros” — embora aqui, é claro, o grupo não
-            seja uma orquestra, mas a igreja.
+            Com a paz virá o descanso. “Ele dá aos seus amados o sono.”
+            Completamente supridos e tranqüilizados por Deus, os crentes
+            descansam em calmo repouso.
           </p>
 
           <p className="texto">
-            Embora possamos ter uma variedade de respostas ao conceito de
-            submissão, devemos ter conhecimento de que a Bíblia o usa de maneira
-            direta e frequente. Para Paulo, a unidade e a saúde da igreja
-            dependiam de os cristãos entenderem a submissão corretamente e
-            colocarem isso em prática entre si.
+            Esse descanso será seguro. Uma coisa é repousar, outra bem diferente
+            é “repousar em segurança”. Fomos trazidos à terra da promessa, à
+            casa do Pai, à câmara do amor, ao seio de Cristo. Com certeza, agora
+            podemos “repousar em segurança”. Descansar em paz é mais seguro para
+            o crente do que assentar-se e inquietar-se.
           </p>
 
           <p className="texto">
-            Como é levar a sério a questão da submissão mútua dos crentes? Em
-            parte, significa que cada um de nós percebe não ter o menor motivo
-            para se sentir excessivamente satisfeito consigo mesmo ou superior a
-            outra pessoa. Em outras palavras, demonstramos submissão mútua
-            revestindo-nos de humildade. Isso é dificultado, é claro, pelo nosso
-            orgulho — um grande desafio que todos enfrentamos e é intensificado
-            por vivermos em uma cultura que está constantemente nos pressionando
-            para a dianteira.
-          </p>
-
-          <p className="texto">
-            No entanto, a igreja deve se destacar nesse tipo de ambiente. Como
-            povo de Deus, entendemos que não podemos nem sequer acordar de manhã
-            sem a capacitação dele. O fato é que somos totalmente dependentes
-            dele (At 17.24-25). O Evangelho é a chave para a verdadeira
-            humildade, pois nos lembra que Deus fez por nós em Jesus aquilo de
-            que mais precisamos e que somos totalmente incapazes de fazer por
-            nós mesmos.
-          </p>
-
-          <p className="texto">
-            A verdadeira humildade não é autodepreciação; é liberdade de nós
-            mesmos. É a liberdade de sermos nós mesmos e nos esquecermos de nós
-            mesmos. É a liberdade que vem de saber que não somos o centro do
-            universo. Quando você mantém tal humildade em vista, está preparado
-            para se submeter aos outros — para trazer tudo o que você é e usá-lo
-            para servir ao bem maior, sob a direção de outros, com os interesses
-            dos outros como sua prioridade. Então sua igreja pode produzir algo
-            bonito — uma comunidade que exibe o Evangelho. Portanto, não espere
-            que os outros em sua igreja sejam esse tipo de cristão. Hoje,
-            humildemente, decida que você será esse cristão.
+            “Ele me faz repousar em pastos verdejantes” (Sl 23.2). Nunca
+            descansaremos enquanto o Consolador não nos fizer repousar.
           </p>
           <p>
             Fonte:{" "}
-            <a href="https://voltemosaoevangelho.com/blog/devocionais/submissao-e-humildade-efesios-5-20-21/" target="_blank" className="fonte-url">
-              https://voltemosaoevangelho.com/blog/devocionais/submissao-e-humildade-efesios-5-20-21/
+            <a
+              href="https://voltemosaoevangelho.com/blog/devocionais/4-de-janeiro-calmo-repouso//"
+              target="_blank"
+              className="fonte-url"
+            >
+              https://voltemosaoevangelho.com/blog/devocionais/4-de-janeiro-calmo-repouso/
             </a>
           </p>
         </section>
