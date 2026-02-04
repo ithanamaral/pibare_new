05722279c5f9077a0e52d85061cf30b46549234f
Íthan P. Amaral <ithanamaral@gmail.com>
2026-02-04 13:35:04 -0300

    atualiza devocional

--- src/components/devotionals.jsx
+++ src/components/devotionals.jsx
@@ -5,50 +5,91 @@ function Devotionals() {
   return (
     <>
       <main className="devotionals">
-        <h1>A Fé Vê o Arco-íris (Génesis 9 .14)</h1>
+        <h1>Cinco propósitos do sofrimento (Romanos 8 .28)</h1>
         <p className="reflexao">
-          "Sucederá que, quando eu trouxer nuvens sobre a terra, e nelas
-          aparecer o arco."
+          "Sabemos que todas as coisas cooperam para o bem daqueles que amam a
+          Deus, daqueles que são chamados segundo o seu propósito.
         </p>
         <section className="text">
           <p className="texto">
-            Até hoje existem muitas nuvens escuras, mas não tememos que o mundo
-            será destruído por um dilúvio. Vemos o arco-íris freqüentemente nos
-            prevenindo, para que não tenhamos esse temor. A aliança que Deus
-            estabeleceu com Noé permanece, e não devemos nutrir qualquer dúvida
-            a respeito dela. Por que, então, devemos pensar que as nuvens de
-            aflição que agora escurecem nosso céu terminarão em nossa
-            destruição? Rejeitemos esses pensamentos desonrosos e sem
-            fundamentos.
+            Raramente nós conhecemos as micro razões de nossos sofrimentos, mas
+            a Bíblia nos dá macro razões que sustentam a fé.
           </p>
 
           <p className="texto">
-            A fé sempre vê o arco-íris da promessa da aliança, onde quer que
-            nossos sentimentos percebam as nuvens de aflição. Deus tem um arco
-            com o qual Ele poderia atirar suas flechas de destruição. Porém,
-            veja que o arco-íris está voltado par~ cima; é um arco que está
-            destituído de corda ou de flechas. E um arco pendurado apenas para
-            ser visto e não mais utilizado para a guerra. É um arco de várias
-            cores, expressando alegria e deleite, e não um arco avermelhado por
-            carnificina, ou um arco escurecido por ira.
+            É bom termos uma maneira de nos lembrar de algumas delas para que,
+            quando de repente formos afligidos ou tenhamos a chance de auxiliar
+            outros em sua aflição, possamos lembrar de algumas das verdades que
+            Deus nos deu para nos ajudar a não perdermos a esperança.
           </p>
 
           <p className="texto">
-            Tenhamos bom ânimo. Deus jamais escureceu nosso céu a ponto de
-            deixar sua aliança sem uma testemunha; e, mesmo que o tenha feito,
-            devemos confiar nEle, visto que Ele não muda, não mente e não falha,
-            de maneira alguma, em manter sua aliança de paz. Até que as águas
-            inundem a terra novamente, não temos qualquer motivo para duvidar de
-            nosso Deus.
+            Aqui está uma maneira de lembrar: cinco palavras (ou se ajudar,
+            escolha três e tente embra-las).
+          </p>
+          <p className="texto">
+            Os macro propósitos de Deus em nossos sofrimentos incluem:
+          </p>
+
+          <p className="texto">
+            Arrependimento: O sofrimento é um chamado para que nós e outros
+            deixemos de valorizar qualquer coisa na terra acima de Deus. Lucas
+            13.4-5:
+          </p>
+          <p className="texto">
+            “Ou cuidais que aqueles dezoito sobre os quais desabou a torre de
+            Siloé e os matou eram mais culpados que todos os outros habitantes
+            de Jerusalém? Não eram, eu vo-lo afirmo; mas, se não vos
+            arrependerdes, todos igualmente perecereis.”
+          </p>
+          <p className="texto">
+            Confiança: O sofrimento é um chamado para confiar em Deus e não nos
+            suportes que sustentam a vida do mundo. 2Coríntios 1.8-9:
+          </p>
+          <p className="texto">
+            “Porque não queremos, irmãos, que ignoreis a natureza da tribulação
+            que nos sobreveio na Ásia, porquanto foi acima das nossas forças, a
+            ponto de desesperarmos até da própria vida. Contudo, já em nós
+            mesmos, tivemos a sentença de morte, para que não confiemos em nós,
+            e sim no Deus que ressuscita os mortos.”
+          </p>
+          <p className="texto">
+            Justiça: O sofrimento é a disciplina do nosso amoroso Pai celestial,
+            para que possamos compartilhar sua santidade. Hebreus 12.6, 10-11:
+          </p>
+          <p className="texto">
+            “Porque o Senhor corrige a quem ama e açoita a todo filho a quem
+            recebe… Deus, porém, nos disciplina para aproveitamento, a fim de
+            sermos participantes da sua santidade. Toda disciplina, com efeito,
+            no momento não parece ser motivo de alegria, mas de tristeza; ao
+            depois, entretanto, produz fruto pacífico aos que têm sido por ela
+            exercitados, fruto de justiça.”
+          </p>
+          <p className="texto">
+            Galardão: O sofrimento está operando para nós um grande galardão no
+            céu que compensará mil vezes cada perda aqui. 2Coríntios 4.17:
+          </p>
+          <p className="texto">
+            “Porque a nossa leve e momentânea tribulação produz para nós eterno
+            peso de glória, acima de toda comparação.”
+          </p>
+          <p className="texto">
+            Lembrança: O sofrimento nos lembra que Deus enviou seu Filho ao
+            mundo para sofrer, para que o nosso sofrimento não fosse uma
+            condenação divina, mas a sua purificação. Filipenses 3.10:
+          </p>
+          <p className="texto">
+            “…para o conhecer, e o poder da sua ressurreição, e a comunhão dos
+            seus sofrimentos.”
           </p>
           <p>
             Fonte:{" "}
             <a
-              href="https://voltemosaoevangelho.com/blog/devocionais/11-de-janeiro-a-fe-ve-o-arco-iris/"
+              href="https://voltemosaoevangelho.com/blog/devocionais/31-de-janeiro-cinco-propositos-do-sofrimento/?utm_source=newsletter&utm_medium=email&utm_term=Sun+01+Feb+2026&utm_campaign=31+de+Janeiro+Cinco+prop%C3%B3sitos+do+sofrimento+Devocional+Alegria+Inabal%C3%A1vel"
               target="_blank"
               className="fonte-url"
             >
-              https://voltemosaoevangelho.com/blog/devocionais/11-de-janeiro-a-fe-ve-o-arco-iris/
+              https://voltemosaoevangelho.com/blog/devocionais/31-de-janeiro-cinco-propositos-do-sofrimento/
             </a>
           </p>
         </section>
