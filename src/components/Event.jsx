0d5c066b684ef0d5ba9f7731b12a09c13738e07b
Íthan P. Amaral <ithanamaral@gmail.com>
2026-02-07 18:37:42 -0300

    conserta aba de eventos

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -9,13 +9,23 @@ function Event() {
       <h1 className="event-title">Próximos Eventos</h1>
       
       <div className="event-container">
-        {/* Envolvemos a imagem em um link para download */}
           <img src={imagem} alt="Imagem do evento" className="event-image" />
+      </div>
+      <div className="event-info">
+        <h2 className="event-info-title">Imersão na Provisão - 2026</h2>
+        <p className="event-info-description">
+          Junte-se a nós para uma experiência transformadora de fé e comunhão!
+        </p>
+        <ul className="event-info-details">
+          <li><strong>Localização:</strong> Sítio do Rodrigo e Luana</li>
+          <li><strong>Data:</strong> 17 de Fevereiro de 2026</li>
+          <li><strong>Horário:</strong> 8h</li>
+        </ul>
+      </div>
           <a href={imagem} download="evento-imersao" className="download-link">
           <ArrowDownToLine />
-          Clique aqui para baixar a imagem
+          <p className='teste'>Clique aqui para baixar a imagem</p>
           </a>
-      </div>
     </main>
   );
 }
